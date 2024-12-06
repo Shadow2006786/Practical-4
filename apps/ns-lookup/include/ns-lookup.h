87ae7d6911012b7d326f3bedc8bb4cb50917e01d
Borzua <36938572+Shadow2006786@users.noreply.github.com>
2024-12-06 16:11:09 +0200

    Add files via upload

---
+++ apps/ns-lookup/include/ns-lookup.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "stdio.h"
+#include "string.h"
+#include "net-utils.h"
+//#include "winsock2.h"
+//#include "windows.h"
+
+
+int start(int argc, char* argv[]);
+
+int resolve_dns(const char* name);
